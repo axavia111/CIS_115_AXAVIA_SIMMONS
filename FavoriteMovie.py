888cc6333f94e10189511658ddefcbd308a568fa
SANDHILLS\LH218-17 <jad.talbert@gmail.com>
2026-01-27 09:01:01 -0500

    Fav Movie

--- FavoriteMovie.py
+++ FavoriteMovie.py
@@ -1,7 +1,7 @@
 # This program shows my favorite movie with the main characters
 
 favorite_movie = "The Lion King"
-main_characters = "Simba, Mufasa, Scar"
+main_characters = "Simba, Mufasa, and Scar"
 
 
 print(f'My favorite movie is: {favorite_movie}')
