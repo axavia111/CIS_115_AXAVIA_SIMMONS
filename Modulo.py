9f039d4c96f2227e1a57275c8def2c06245d25e0
SANDHILLS\LH218-17 <jad.talbert@gmail.com>
2026-02-04 09:46:20 -0500

    Week 4 Labs

---
+++ Modulo.py
@@ -0,0 +1,14 @@
+#This program shows how to use Modulo
+
+num1 = 15
+
+num2 = 2
+
+result = num1 % num2
+
+if(result % 2 == 0):
+    print("Even")
+else:
+    print("Odd")
+
+    print(result)
---
+++ ThreeIntegers.py
@@ -0,0 +1,15 @@
+
+num1 = int(input('Enter first number: '))
+
+num2 = int(input('Enter second number: '))
+
+num3 = int(input('Enter third number: '))
+
+result = float(num1 + num2) / float(num3)
+
+if(result > 0):
+    print("The mathematical operation is > 0")
+else:
+    print("The mathematical operation is <= 0")
+
+    print(result)
