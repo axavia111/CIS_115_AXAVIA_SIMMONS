ef9c71fe851701fd544d40981273d818c5dec187
SANDHILLS\LH218-17 <jad.talbert@gmail.com>
2026-02-11 09:46:10 -0500

    codes

---
+++ myFirstForLoop.py
@@ -0,0 +1,6 @@
+#this program prompts user to enter a number to itterate over a for-in loop
+
+numOfLoops = int(input("Enter the number of loops you would like to execute: "))
+
+for i in range(numOfLoops):
+    print (i)
