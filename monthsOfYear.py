d324cf43997cf4ff3a29cf997fd472b95741be31
SANDHILLS\LH218-17 <jad.talbert@gmail.com>
2026-02-26 10:50:16 -0500

    codes

--- monthsOfYear.py
+++ monthsOfYear.py
@@ -1,6 +1,23 @@
+#This program allows user input to give a start and end month
+
+startMonth = int(input("Enter Start Month: "))
+endMonth = int(input("Enter End Month: "))
+
+def months_of_year(startMonth,endMonth):
+    months = ['January','Febuary','March','April','May','June','July','August','September','October','November','December']
+    return months[startMonth], months[endMonth]
 
 
-months = ['January','Febuary','March','April','May','June','July']
 
-def months_of_year():
-    pass
+
+startMonth, endMonth = months_of_year(startMonth,endMonth)
+
+print(f'The Start Month is  {startMonth} and the End Month is {endMonth}')
+
+    
+    
+    
+
+
+
+    
