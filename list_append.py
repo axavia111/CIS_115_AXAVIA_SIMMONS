da29e7a21f884a851c843fb43a6de6de31cbebed
SANDHILLS\LH218-17 <jad.talbert@gmail.com>
2026-03-03 09:52:17 -0500

    codes

--- list_append.py
+++ list_append.py
@@ -6,11 +6,13 @@ def append_to_list(value):
     list.append(value)
     return list
 
-x = input("Would you like to enter another value to append to the list?")
+x = input("Would you like to enter another value to append to the list?: ")
 
 while x  == 'y':
     value = input("Enter a value: ")
-    x = input("Would you like to enter another value to append to the list?")
+    x = input("Would you like to enter another value to append to the list?: ")
     append_to_list(value)
+
+
 print(list)
 
