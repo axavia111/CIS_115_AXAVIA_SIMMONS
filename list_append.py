d2700fd1b7c493146b993fa94e32abf253c9644c
SANDHILLS\LH218-17 <jad.talbert@gmail.com>
2026-03-04 09:48:54 -0500

    codes

--- list_append.py
+++ list_append.py
@@ -1,18 +1,22 @@
+#This program allows user to append to a list
 
-value = input("Enter a value: ")
-list = [1,2,3,4,5,6]
-def append_to_list(value):
-    
-    list.append(value)
+
+list = [1,2,3,4,5,6] #The list is already defined
+
+def append_to_list(value,list): #This function passes the list and the value entered; Then the value is then added to the list at the end
+    list.append(value)   
     return list
 
-x = input("Would you like to enter another value to append to the list?: ")
+value = input("Enter a value: ") #Prompt user to enter any value
+append_to_list(value,list)
 
-while x  == 'y':
+
+x = input("Would you like to enter another value to append to the list?:  ") #Prompts user again to enter a value
+
+while x  == 'yes': #Uses a while-loop and keeps prompting user to enter a value to add to the list. 
     value = input("Enter a value: ")
-    x = input("Would you like to enter another value to append to the list?: ")
-    append_to_list(value)
+    append_to_list(value,list)
+    x = input("Would you like to enter another value to append to the list?:  ") #Once x == no, then the loop stops.
 
-
-print(list)
+print(list) #Prints out the list with the values user inputted
 
---
+++ slice_list.py
@@ -0,0 +1,10 @@
+
+
+months = ['January','Feburary','March','April','May','June','July','August','September','October','November','December']
+
+def slice_list(months):
+    return months[4:7]
+
+
+print(slice_list(months))
+
