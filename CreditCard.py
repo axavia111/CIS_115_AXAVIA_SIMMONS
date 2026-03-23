c03c34d78503d7142ce60ae6769f709d94626f35
SANDHILLS\LH218-17 <jad.talbert@gmail.com>
2026-03-23 09:43:33 -0400

    codes

---
+++ CreditCard.py
@@ -0,0 +1,13 @@
+#This program checks whether a credit card number is valid or invalid
+
+ccNum = int(input("Enter credit card number: "))
+
+ccNum = ccNum[::-1]
+
+def validateCreditCard(ccNum):
+    
+    for number in ccNum:
+
+
+
+        pass
