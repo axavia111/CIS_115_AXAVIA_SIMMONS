970b2cb6aeaadf6bc9b2e344c4cf28c4ebc93294
SANDHILLS\LH218-17 <jad.talbert@gmail.com>
2026-03-25 09:47:18 -0400

    codes

--- CreditCard.py
+++ CreditCard.py
@@ -1,7 +1,7 @@
 #This program checks whether a credit card number is valid or invalid
 
-ccNum = (input("Enter credit card number: ")) #Prompts user to enter credit card number, needs to be string value in oder to loop over each character
-
+ccNum = input("Enter credit card number: ") #Prompts user to enter credit card number, needs to be string value in order to loop over each character
+x = "Yes"
 ccNum = ccNum[::-1] #reverses credit card number
 
 def validateCreditCard(ccNum): #passes the reversed number through
@@ -17,10 +17,28 @@ def validateCreditCard(ccNum): #passes the reversed number through
             if num > 9: #If number multiplied by 2 is greater than nine, than subtract the product by 9
                 num = (num * 2) - 9
     
-    total = total + num
-    count = count + 1
+        total = total + num #Adds the product of the number passed through the loop to the total
+        count = count + 1 #Adds the each time it loops over
 
-    return total % 10 == 0 #returns the mod 10 check if equals to 0
+    return total % 10 == 0 #returns the mod 10 check if the sum of all numbers is equal 0
 
-if validateCreditCard(ccNum):
-    print(f'The credit card entered is valid!!!!')
+if validateCreditCard(ccNum): #The credit card number is valid and stops the loop.
+    print(f'The credit card {ccNum} is valid!!!')
+
+else:
+    print(f'The credit card {ccNum} is invalid. Please try again.') #The credit card number entered is invaild and prompts user to enter another number.
+        
+        
+while x == "Yes":
+            x = input("Would you like to enter credit card number?: ") 
+
+            ccNum = input("Enter a credit card number: ") #Keeping the loop going until a valid credit card number is entered
+            ccNum = ccNum[::-1]
+
+            validateCreditCard(ccNum) #calls on function to check the next number
+    
+
+
+
+
+    
