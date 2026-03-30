724526deea0c9cc6c0c791b75137072aa0015e13
SANDHILLS\LH218-17 <jad.talbert@gmail.com>
2026-03-30 09:47:03 -0400

    codes

---
+++ FinalProject.py
@@ -0,0 +1,19 @@
+
+print('-------------------------------------------')
+print('            PRODUCT CATALOG                ')
+print('-------------------------------------------')
+print('1  | USB Drive (128 GB)   |  $12.00')
+print('2  | Mac Book Pro (15 in.)|  $2900.00')
+print('3  | Arduino 1010 (w/Bluetooth)   |  $48.00')
+print('4  | Ring Camera (wireless)   |  $156.00')
+print('5  | Smart TV (TCL 70 in.)   |  $359.00')
+
+
+
+
+
+
+
+
+
+ListofProducts = {}
