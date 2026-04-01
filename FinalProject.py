723018d998eca5725fe178640001213556d6ce09
SANDHILLS\LH218-17 <jad.talbert@gmail.com>
2026-04-01 09:46:04 -0400

    codes

--- FinalProject.py
+++ FinalProject.py
@@ -2,11 +2,49 @@
 print('-------------------------------------------')
 print('            PRODUCT CATALOG                ')
 print('-------------------------------------------')
-print('1  | USB Drive (128 GB)   |  $12.00')
-print('2  | Mac Book Pro (15 in.)|  $2900.00')
+print('1  | USB Drive (128 GB)           |  $12.00')
+print('2  | Mac Book Pro (15 in.)        |  $2900.00')
 print('3  | Arduino 1010 (w/Bluetooth)   |  $48.00')
-print('4  | Ring Camera (wireless)   |  $156.00')
-print('5  | Smart TV (TCL 70 in.)   |  $359.00')
+print('4  | Ring Camera (wireless)       |  $156.00')
+print('5  | Smart TV (TCL 70 in.)        |  $359.00')
+
+
+ListofProducts = [{
+    'Product ID': '1',
+    'SKU': 'usb_k981',
+    'Price': '$12.00',
+    'Qty': '',
+    'Description': 'USB 128GB drive.',
+    'Qty on Hand': '1000'},
+    
+    {'Product ID': '2',
+    'SKU': 'mbpro_490',
+    'Price': '$2900.00',
+    'Qty': '',
+    'Description': 'Mac Book Pro 15 inch.',
+    'Qty on Hand': '45'},
+   
+    { 'Product ID': '3',
+    'SKU': 'chip_1010',
+    'Price': '$48.00',
+    'Qty': '',
+    'Description': 'Arduino microprocessor',
+    'Qty on Hand': '325'},
+    
+    {'Product ID': '4',
+    'SKU': 'cam_78',
+    'Price': '$156.00',
+    'Qty': '',
+    'Description': 'Ring Camera Model 78.',
+    'Qty on Hand': '98'},
+    
+    {'Product ID': '5',
+    'SKU': 'smt_tv_100',
+    'Price': '$359.00',
+    'Qty': '',
+    'Description': 'TCL Smart TV.',
+    'Qty on Hand': '225'
+    }]
 
 
 
@@ -14,6 +52,3 @@ print('5  | Smart TV (TCL 70 in.)   |  $359.00')
 
 
 
-
-
-ListofProducts = {}
